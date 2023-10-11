f10af48926c0c506522044f019c8032263378062
Enrico Guiraud <enrico.guiraud@pm.me>
2023-10-11 16:28:08 -0600

    Large refactoring: play well with a JAX workflow
    Instead of applying `jax.grad` internally, expose
    `evaluate` and `eval_dict` functions that play
    well with `jax.grad` and `jax.jit`.

--- src/correctionlib_gradients/_base.py
+++ src/correctionlib_gradients/_base.py
@@ -2,25 +2,19 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 from functools import partial
-from typing import Any, Callable, Optional, TypeAlias
+from typing import TypeAlias
 
+import correctionlib.schemav2 as schema
 import jax
 import numpy as np
-from correctionlib.schemav2 import Content, Correction
 
-Input: TypeAlias = float
-Output: TypeAlias = tuple[float, dict[str, float]]
-
-
-def isfloat(x: Any) -> bool:
-    if isinstance(x, float):
-        return True
-    if hasattr(x, "dtype") and np.issubdtype(x.dtype, np.floating):
-        return True  # covers numpy and jax arrays
-    return False
+# TODO: switch to use numpy.array_api.Array as _the_ array type.
+# Must wait for it to be out of experimental.
+# See https://numpy.org/doc/stable/reference/array_api.html.
+Value: TypeAlias = float | np.ndarray | jax.Array
 
 
-def apply_ast(ast: Content, correction_name: str, _inputs: dict[str, Input]) -> float:
+def apply_ast(ast: schema.Content, correction_name: str, _inputs: dict[str, Value]) -> Value:
     match ast:
         case float(x):
             return x
@@ -32,50 +26,24 @@ def apply_ast(ast: Content, correction_name: str, _inputs: dict[str, Input]) -> 
             raise NotImplementedError(msg)
 
 
-def grad_wrt_key(
-    f: Callable[[dict[str, Input]], float], wrt: list[str] | None
-) -> Callable[[dict[str, float]], tuple[float, dict[str, float]]]:
-    """Given a callable f that takes a dictionary inputs, return a callable
-    that evaluates the gradient of f w.r.t. one or more of the arrays in
-    the dictionary (specified by key).
-
-    If wrt is None, gradients are taken w.r.t. all floating point inputs.
-    """
-
-    def grad_wrt_impl(inputs: dict[str, float]) -> tuple[float, dict[str, float]]:
-        in_vars = list(inputs.keys())
-        true_wrt = wrt if wrt is not None else [k for k, v in inputs.items() if isfloat(v)]
-        argnums = [in_vars.index(wrt_var) for wrt_var in true_wrt]
-
-        def f_with_positionals(*args: Input) -> float:
-            args_as_dict = dict(zip(in_vars, args))
-            return f(args_as_dict)
-
-        # TODO should we cache df instead of calling value_and_grad at every invocation?
-        df = jax.value_and_grad(f_with_positionals, argnums)
-        value, grads = df(*inputs.values())
-        return value, dict(zip(true_wrt, grads))
-
-    return grad_wrt_impl
-
-
 class CorrectionWithGradient:
-    def __init__(self, c: Correction, *, wrt: Optional[list[str]] = None, jit: bool = False):
-        if len(c.inputs) == 0:
-            msg = "Cannot take gradients of a correction that has no input variables"
-            raise ValueError(msg)
-
-        self._grad_evaluator = grad_wrt_key(partial(apply_ast, c.data, c.name), wrt=wrt)
-        if jit:
-            self._grad_evaluator = jax.jit(self._grad_evaluator)
+    def __init__(self, c: schema.Correction):
+        self._evaluator = partial(apply_ast, c.data, c.name)
         self._input_names = [v.name for v in c.inputs]
         self._name = c.name
 
-    def evaluate(self, inputs: dict[str, Input]) -> Output:
+    def evaluate(self, *inputs: Value) -> Value:
+        if (n_in := len(inputs)) != (n_expected := len(self._input_names)):
+            msg = f"This correction requires {n_expected} input(s), {n_in} provided"
+            raise ValueError(msg)
+
+        input_dict = dict(zip(self._input_names, inputs))
+        return self._evaluator(input_dict)
+
+    def eval_dict(self, inputs: dict[str, Value]) -> Value:
         for n in self._input_names:
             if n not in inputs:
                 msg = f"Variable '{n}' is required by correction '{self._name}' but is not present in input"
                 raise ValueError(msg)
 
-        value, grads = self._grad_evaluator(inputs)
-        return (float(value), grads)
+        return self._evaluator(inputs)
