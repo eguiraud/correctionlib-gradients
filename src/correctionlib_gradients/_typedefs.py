ce9952ac4a85097955c1222e33312f649cf01d84
Enrico Guiraud <enrico.guiraud@pm.me>
2023-10-24 17:47:47 -0600

    Allow lists of floats as Values

--- src/correctionlib_gradients/_typedefs.py
+++ src/correctionlib_gradients/_typedefs.py
@@ -6,5 +6,5 @@ import numpy as np
 # TODO: switch to use numpy.array_api.Array as _the_ array type.
 # Must wait for it to be out of experimental.
 # See https://numpy.org/doc/stable/reference/array_api.html.
-Array: TypeAlias = np.ndarray | jax.Array
+Array: TypeAlias = np.ndarray | jax.Array | list[float]
 Value: TypeAlias = float | Array
