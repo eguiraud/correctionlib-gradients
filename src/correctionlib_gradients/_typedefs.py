c628aff011a49c389fa39e140c92f162d94f87c7
Enrico Guiraud <enrico.guiraud@pm.me>
2023-10-27 16:31:14 -0600

    Always use jax.Arrays internally

--- src/correctionlib_gradients/_typedefs.py
+++ src/correctionlib_gradients/_typedefs.py
@@ -6,5 +6,4 @@ import numpy as np
 # TODO: switch to use numpy.array_api.Array as _the_ array type.
 # Must wait for it to be out of experimental.
 # See https://numpy.org/doc/stable/reference/array_api.html.
-Array: TypeAlias = np.ndarray | jax.Array | list[float]
-Value: TypeAlias = float | Array
+Value: TypeAlias = float | np.ndarray | jax.Array | list[float]
