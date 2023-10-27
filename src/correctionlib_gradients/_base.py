e30cc8ac086b3367a0c85fb3f03fb010b50e7878
Enrico Guiraud <enrico.guiraud@pm.me>
2023-10-26 20:45:16 -0600

    Move type aliases to their own file
    They will soon be need to be shared across modules.

--- src/correctionlib_gradients/_base.py
+++ src/correctionlib_gradients/_base.py
@@ -9,11 +9,7 @@ import jax
 import numpy as np
 from scipy.interpolate import CubicSpline  # type: ignore[import-not-found]
 
-# TODO: switch to use numpy.array_api.Array as _the_ array type.
-# Must wait for it to be out of experimental.
-# See https://numpy.org/doc/stable/reference/array_api.html.
-Array: TypeAlias = np.ndarray | jax.Array
-Value: TypeAlias = float | Array
+from correctionlib_gradients._typedefs import Array, Value
 
 
 def midpoints(x: Array) -> Array:
