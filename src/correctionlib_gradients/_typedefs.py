e0b56e3fc6cc95fd4a037401ac63d010e151f227
Enrico Guiraud <enrico.guiraud@pm.me>
2023-11-30 10:11:08 -0600

    Fix mypy error with latest version of mypy

--- src/correctionlib_gradients/_typedefs.py
+++ src/correctionlib_gradients/_typedefs.py
@@ -1,9 +1,9 @@
-from typing import TypeAlias
+from typing import Any, TypeAlias
 
 import jax
-import numpy as np
+import numpy.typing
 
 # TODO: switch to use numpy.array_api.Array as _the_ array type.
 # Must wait for it to be out of experimental.
 # See https://numpy.org/doc/stable/reference/array_api.html.
-Value: TypeAlias = float | np.ndarray | jax.Array | list[float]
+Value: TypeAlias = float | numpy.typing.NDArray[Any] | jax.Array | list[float]
