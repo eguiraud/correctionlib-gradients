f07fb73d00a5c5ff5b2261a1f33c9c73555a77f7
Enrico Guiraud <enrico.guiraud@pm.me>
2023-12-01 08:50:47 -0600

    Rename _base.py to _correction_with_gradient.py
    Also rename tests accordingly.

--- src/correctionlib_gradients/__init__.py
+++ src/correctionlib_gradients/__init__.py
@@ -3,6 +3,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 from correctionlib_gradients.__about__ import __version__
-from correctionlib_gradients._base import CorrectionWithGradient
+from correctionlib_gradients._correction_with_gradient import CorrectionWithGradient
 
 __all__ = ["CorrectionWithGradient", "__version__"]
