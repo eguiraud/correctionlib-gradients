6d582a9b96ce84f1b170b8c78a3d7f53e0e4b748
ligerlac <lino.oscar.gerlach@cern.ch>
2025-02-14 10:37:21 +0100

    wrote tests for new category support

--- tests/test_correction_with_gradient.py
+++ tests/test_correction_with_gradient.py
@@ -180,16 +180,38 @@ schemas = {
             flow="clamp",
         ),
     ),
-    # this type of correction is unsupported
-    "categorical": schemav2.Correction(
-        name="categorical",
+    # can be differentiated w.r.t. x, but not c
+    "categorical-with-formula": schemav2.Correction(
+        name="categorical-with-formula",
         version=2,
-        inputs=[schemav2.Variable(name="c", type="int")],
-        output=schemav2.Variable(name="weight", type="real"),
+        inputs=[
+            schemav2.Variable(name="x", type="real"),
+            schemav2.Variable(name="c", type="int"),
+        ],
+        output=schemav2.Variable(name="a scale", type="real"),
         data=schemav2.Category(
             nodetype="category",
-            input="x",
-            content=[schemav2.CategoryItem(key=0, value=1.234)],
+            input="c",
+            content=[
+                schemav2.CategoryItem(
+                    key=1,
+                    value=schemav2.Formula(
+                        nodetype="formula",
+                        variables=["x"],
+                        parser="TFormula",
+                        expression="42*x",
+                    ),
+                ),
+                schemav2.CategoryItem(
+                    key=-1,
+                    value=schemav2.Formula(
+                        nodetype="formula",
+                        variables=["x"],
+                        parser="TFormula",
+                        expression="1337*x",
+                    ),
+                ),
+            ]
         ),
     ),
     # this type of correction is unsupported
@@ -232,11 +254,6 @@ def test_missing_input():
         ValueError, match="This correction requires 1 input\\(s\\), 0 provided. Required inputs are \\['x'\\]"
     ):
         cg.evaluate()
-
-
-def test_unsupported_correction():
-    with pytest.raises(ValueError, match="Correction 'categorical' contains the unsupported operation type 'Category'"):
-        CorrectionWithGradient(schemas["categorical"])
 
 
 def test_unsupported_flow_type():
@@ -518,3 +535,28 @@ def test_compound_binning_with_formularef():
     value, grad = jax.value_and_grad(cg.evaluate)(0.5)
     assert math.isclose(value, 0.2)
     assert math.isclose(grad, 0.2)
+
+
+def test_categorical_with_formula():
+    cg = CorrectionWithGradient(schemas["categorical-with-formula"])
+
+    value = cg.evaluate(0.5, 1)
+    assert math.isclose(value, 21.0)
+
+    value, grad = jax.value_and_grad(cg.evaluate, argnums=0)(1.0, 1)
+    assert math.isclose(value, 42.0)
+    assert math.isclose(grad, 42.0)
+
+    value = cg.evaluate(1.0, -1)
+    assert math.isclose(value, 1337.0)
+
+    value, grad = jax.value_and_grad(cg.evaluate, argnums=0)(1.0, -1)
+    assert math.isclose(value, 1337.0)
+    assert math.isclose(grad, 1337.0)
+
+    # differenttiating w.r.t. to the category key should not work
+    with pytest.raises(TypeError):
+        value, grad = jax.value_and_grad(cg.evaluate, argnums=1)(1.0, 1)
+
+    with pytest.raises(TypeError):
+        value, grad = jax.value_and_grad(cg.evaluate, argnums=1)(1.0, -1)
